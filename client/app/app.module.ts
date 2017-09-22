c481edbf0f77caf5daedb52f40e435ab31843337
Caue Queiroz <cauenqueiroz@gmail.com>
2017-09-22 13:12:30 -0300

    [Finish module 04] Create another component and routes

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { FotoModule } from './foto/foto.module';
 import { HttpModule } from '@angular/http';
-
 import 'rxjs/add/operator/map';
 
+import { AppComponent } from './app.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { ListagemComponent } from './listagem/listagem.component';
+import { routing } from './app.routes';
+
+import { FotoModule } from './foto/foto.module';
+import { PainelModule } from './painel/painel.module';
+
 @NgModule({
-    imports: [ BrowserModule, FotoModule, HttpModule ],
-    declarations: [ AppComponent ],
+    imports: [ BrowserModule, HttpModule, FotoModule, PainelModule, routing ],
+    declarations: [ AppComponent, CadastroComponent, ListagemComponent ],
     bootstrap: [ AppComponent ]
 })
 export class AppModule {}
