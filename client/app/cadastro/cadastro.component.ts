f3fbf45bfa569b4feea4cc6488906694f3ec02c7
Caue Queiroz <cauenqueiroz@gmail.com>
2017-09-27 10:36:16 -0300

    [Finish module 02] Create injectable service

--- client/app/cadastro/cadastro.component.ts
+++ client/app/cadastro/cadastro.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FotoComponent } from '../foto/foto.component';
-import { Http, Headers } from '@angular/http';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FotoService } from '../foto/foto.service';
 
 @Component({
     moduleId: module.id,
@@ -11,12 +11,12 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class CadastroComponent {
 
     foto: FotoComponent = new FotoComponent();
-    http: Http;
     meuForm: FormGroup;
+    service: FotoService;
 
-    constructor(http: Http, fb: FormBuilder) {
+    constructor(fb: FormBuilder, service: FotoService) {
 
-        this.http = http;
+        this.service = service;
 
         this.meuForm = fb.group({
             titulo: ['', Validators.compose([Validators.required, Validators.minLength(4)])],
@@ -28,14 +28,12 @@ export class CadastroComponent {
     cadastrar(event: Event) {
 
         event.preventDefault();
-        
-        let headers = new Headers();
-        headers.append('Content-Type', 'application/json');
 
-        this.http
-            .post('v1/fotos', JSON.stringify(this.foto), { headers: headers })
-            .subscribe(() => {
-                this.foto = new FotoComponent()
-            }, err => console.log(err));     
+        this.service
+            .cadastrar(this.foto)
+            .subscribe(
+                () => this.foto = new FotoComponent(),
+                error => console.log(error)
+            );            
     }
 }
--- client/app/foto/foto.module.ts
+++ client/app/foto/foto.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { FotoComponent } from './foto.component';
 import { FiltraPorTitulo } from './foto.pipes';
+import { FotoService } from './foto.service';
 
 @NgModule({
     declarations: [ FotoComponent, FiltraPorTitulo ],
-    exports: [ FotoComponent, FiltraPorTitulo ]
+    exports: [ FotoComponent, FiltraPorTitulo ],
+    providers: [ FotoService ]
 })
 export class FotoModule {}
